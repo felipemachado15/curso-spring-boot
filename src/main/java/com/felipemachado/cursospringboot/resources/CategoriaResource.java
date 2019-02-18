d78c1d7b3f61ebc6a924aa8957fa2321268e1b4b
Felipe Machado <felipe.machado.ads@gmail.com>
2019-02-18 17:53:57 -0300

    Configurando JPA,H2 e as dependencias do spring pra configurar o h2

--- src/main/java/com/felipemachado/cursospringboot/resources/CategoriaResource.java
+++ src/main/java/com/felipemachado/cursospringboot/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.felipemachado.cursospringboot.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.felipemachado.cursospringboot.domain.Categoria;
+
+
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	@RequestMapping(method = RequestMethod.GET)
-	public String restOk() {
-		return "Rest funcionando";
+	public List<Categoria> restOk() {
+		Categoria cat1 =new Categoria(1,"informatica");
+		Categoria cat2 =new Categoria(2,"Escritorio");
+		List<Categoria>listaCategorias = new ArrayList<Categoria>();
+		listaCategorias.add(cat1);
+		listaCategorias.add(cat2);
+		return listaCategorias;
+		
 	}
 	
 	
